e53d6e90e9212a1beefa5bf5fd0bf46b8c33c9dd
Chris Nolet <chris@relaunch.io>
2015-03-31 20:31:45 -0700

    Begin adding static header

--- HanaHaus/Controllers/BookingViewController.m
+++ HanaHaus/Controllers/BookingViewController.m
@@ -17,6 +17,38 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
+    [self.navigationController.navigationBar setBackgroundImage:[[UIImage alloc] init]
+                                                  forBarMetrics:UIBarMetricsDefault];
+
+    self.navigationController.navigationBar.shadowImage = [[UIImage alloc] init];
+
+//    self.navigationItem.titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Title.png"]];
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+
+//    [self.navigationController setNavigationBarHidden:YES animated:animated];
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+
+//    [self.navigationController setNavigationBarHidden:NO animated:animated];
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)viewDidLayoutSubviews
+{
+//    CGRect frame = self.navigationController.navigationBar.frame;
+//    frame.size.height = 0.0f;
+//
+//    self.navigationController.navigationBar.frame = frame;
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -29,6 +61,29 @@
     if ([cell respondsToSelector:@selector(setLayoutMargins:)]) {
         cell.layoutMargins = UIEdgeInsetsZero;
         cell.preservesSuperviewLayoutMargins = NO;
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - UIScrollViewDelegate
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    // Keep the balance view pinned (floating) when the user pulls to refresh
+    CGFloat offset = self.tableView.contentOffset.y;
+    NSLog(@"%f", offset);
+    if (offset < 0) {
+
+        // Pulled to refresh
+        self.tableView.tableHeaderView.transform = CGAffineTransformMakeTranslation(0, offset);
+
+    } else {
+
+        // Partially visible
+        if (offset < self.tableView.tableHeaderView.frame.size.height) {
+            self.tableView.tableHeaderView.transform = CGAffineTransformIdentity;
+        }
     }
 }
 
